5a4721acf57113b6d3935cb5c92e5ab47a7541a6
sonya <chupachups_xd@mail.ru>
2021-01-26 23:54:13 +0700

    д/з к уроку 1

---
+++ lesson-1/project/js/main.js
@@ -0,0 +1,20 @@
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
+];
+//Функция для формирования верстки каждого товара
+const renderProduct = (title, price) => {
+    return `<div class="product-item"><h3>${title}</h3>
+                <p>${price}&#8381</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = (list = products) => {
+    const productsList = list.map(item => renderProduct(item.title, item.price)).join(" ");
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList;
+};
+
+renderPage();
